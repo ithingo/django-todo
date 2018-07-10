41541c1ea7288d930784ddd88aca44d55bbd9eda
Vladimir Borodaev <v.borodaev@dunice.net>
2018-07-10 18:12:29 +0300

    Added view functionality and basic template for items

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,15 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import TodoItem
 
 
 def index(request):
-    return HttpResponse('Hello, I am here with the simpliest django view')
+    task_list = TodoItem.objects.order_by('-created_at')[:6]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'task_list': task_list,
+    }
+    return HttpResponse(template.render(context, request))
