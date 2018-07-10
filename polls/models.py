1c8b1f9d929198111bc7748e99899f724082aca9
Vladimir Borodaev <v.borodaev@dunice.net>
2018-07-10 17:30:45 +0300

    Changed model removing unnecessary read-only getters (omg). Changed content name filed from text to input_text

--- polls/models.py
+++ polls/models.py
@@ -3,26 +3,10 @@ from django.utils import timezone as date
 
 
 class TodoItem(models.Model):
-    text = models.TextField(blank=True)
+    input_text = models.TextField(blank=True)
     checked = models.BooleanField(default=False)
     created_at = models.DateField(default=date.now().strftime('%Y-%m-%d'))
     updated_at = models.DateField(default=date.now().strftime('%Y-%m-%d'))
-
-    @property
-    def text(self):
-        """Returns text value had been got from input field"""
-        return '%s' % self.text
-
-    @property
-    def is_checked(self):
-        """Returns status of task"""
-        # return '%s' % self.checked.lower()  # " 'true' instead of 'True' for JS"
-        return self.checked
-
-    @property
-    def item_id(self):
-        """Returns item id from table"""
-        return self.id
 
     class Meta:
         verbose_name = 'TodoItem'
@@ -34,4 +18,4 @@ class TodoItem(models.Model):
         db_table = 'django_todo_items'
 
     def __str__(self):
-        return '%d -- %s' % self.id, self.text
+        return "{0} ({2}) - {1}".format(self.id, self.input_text, self.checked)
